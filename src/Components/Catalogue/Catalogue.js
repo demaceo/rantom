72c39e65d3aa1701323b0eac78362af1ba546b42
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2020-12-14 23:09:03 -0700

    Complete tests for Home component

--- src/Components/Catalogue/Catalogue.js
+++ src/Components/Catalogue/Catalogue.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MovieCard from "../MovieCard/MovieCard";
-import { Col, Row } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import "./Catalogue.scss";
 
--- src/Components/Home/Home.test.js
+++ src/Components/Home/Home.test.js
@@ -1,13 +1,69 @@
 import React from "react";
 import Home from "./Home";
 import { render, screen } from "@testing-library/react";
-import { getMovies } from "../../apiCalls";
+import { MemoryRouter } from "react-router-dom";
 
-describe.skip("Home", () => {
-    it("should render a Catalogue component", () => {
-        render(<Home />);
+const mockMoviesList = {
+  movies: [
+    {
+      id: 1,
+      title: "Movie Title",
+      poster_path: "someURL",
+      backdrop_path: "someURL",
+      release_date: "2019-12-04",
+      overview: "Some overview",
+      average_rating: 6,
+    },
+    {
+      id: 2,
+      title: "Second Movie Title",
+      poster_path: "someURL",
+      backdrop_path: "someURL",
+      release_date: "2019-12-04",
+      overview: "Some overview",
+      average_rating: 4,
+    },
+  ],
+};
+const mockMovieData = {
+  movie: {
+    id: 1,
+    title: "Movie Title",
+    poster_path: "someURL",
+    backdrop_path: "someURL",
+    release_date: "2019-12-04",
+    overview: "Some overview",
+    average_rating: 6,
+    genres: [ "Drama" ],
+    budget: 63000000,
+    revenue: 100853753,
+    runtime: 139,
+    tagline: "Movie Tagline",
+  },
+};
+
+describe("Home", () => {
+    it("should render a Catalogue component with movie cards", () => {
+        render(
+            <MemoryRouter>
+                <Home movies={mockMoviesList.movies}/>
+            </MemoryRouter>
+            );
+        const cards = screen.getAllByRole("article")
+
+        expect(cards[0]).toBeInTheDocument();
     })
+
     it("should render the correct number of movie cards", () => {
-        render(<Home />);
+        render(
+            <MemoryRouter>
+                <Home movies={mockMoviesList.movies}/>
+            </MemoryRouter>
+            );
+
+        const cards = screen.queryAllByRole("article")
+
+
+        expect(cards.length).toEqual(mockMoviesList.movies.length)
     })
 })
